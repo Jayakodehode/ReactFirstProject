991bb7af98b358f63ba6732eb7fb4c38e1223fb0
Jayakodehode <jaya.kodehode@gmail.com>
2023-06-08 14:09:43 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <main>
+        <h1>main</h1>
+      </main>
+      <footer>
+        <h1> footer</h1>
+      </footer>
+    </div>
+  );
+}
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import "./StyleNavbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <p className="logo">Logo</p>
+      </div>
+      {
+        /**
+         * multiline comments
+         */
+        //single line comments
+      }
+      <div className="navbar-items">
+        <ul className="navbar-list">
+          <li className="listitems">
+            <a href=""> Home</a>
+          </li>
+          <li className="listitems">
+            <a href="">About</a>
+          </li>
+          <li className="listitems">
+            <a href="">Projects</a>
+          </li>
+          <li className="listitems">
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}

deb1464854a3f61698b4b7601ce0f8506b7d3adf
Jayakodehode <jaya.kodehode@gmail.com>
2023-06-16 23:52:15 +0200

    commit changes

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,22 +1,26 @@
-export function Article(props) {
-  const { title, description, body, author } = props;
-  console.log(props);
+export function Article({ title, description, author, createdAt }) {
   return (
     <>
-      <h1>{title}</h1>
+      <h1>{author.username}</h1>
+      <p>{createdAt}</p>
+      <h3>{title}</h3>
       <p>{description}</p>
-
-      <Article1 body={props.body} author={author.username} />
+      <Article1
+        username={author.username}
+        bio={author.bio}
+        image={author.image}
+      />
+      <Article1 {...author} />
     </>
   );
 }
 
-function Article1({ author, body }) {
-  console.log(body, author);
+function Article1({ username, bio, image }) {
   return (
     <>
-      <h1>{author}</h1>
-      <p>{body}</p>
+      <h1>{username}</h1>
+      <p>{bio}</p>
+      <img src={image} alt="" />
     </>
   );
 }
---
+++ src/assets/ArticleListArray.jsx
@@ -0,0 +1,22 @@
+export const ArticleListArray = [
+  {
+    id: "1234",
+    name: "Jaya",
+    degree: "bachelor in computers",
+  },
+  {
+    id: "1235",
+    name: "JC",
+    degree: "Phd in cloud",
+  },
+  {
+    id: "1236",
+    name: "Barsha",
+    degree: "Masters in law",
+  },
+  {
+    id: "1237",
+    name: "Antorweep",
+    degree: "Assosiate professer",
+  },
+];
